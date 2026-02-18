5f41284104237f591143a6a2ed6f12a57c44df83
PC5 <diegosf9009@gmail.com>
2026-02-17 20:43:07 -0600

    Pokemon1

---
+++ Assets/Scripts/DestroyInSeconds.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DestroyInSeconds : MonoBehaviour
+{
+   
+    [SerializeField]
+
+    private float secondsToDestroy = 1f;
+
+    private void OnEnable() 
+    
+    {
+
+        Invoke(nameof(DestroySelf), secondsToDestroy);
+
+    }
+
+        private void DestroySelf() 
+        
+        
+        {
+            gameObject.SetActive(false);
+
+        }
+
+
+
+}
